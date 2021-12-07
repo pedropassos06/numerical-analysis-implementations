93d67687ca1bf6ea0dac22d26242b0f53fdc971b
pedropassos06 <64121400+pedropassos06@users.noreply.github.com>
2021-12-07 18:01:37 -0500

    Update bisection_method.m

--- bisection_method.m
+++ bisection_method.m
@@ -1,8 +1,7 @@
 function [c,fVal,iter] = bisectAlg(f, a, b, tol1, tol2)
-format long
 iter = 0;
 c = (a + b) / 2;
-while abs(b-a) > tol2 && abs(f(c)) > tol1
+while (abs(b-a) > tol2 && abs(f(c)) > tol1)
     iter = iter + 1;
     if f(a) * f(c) > 0
         a = c;
